d0883cfed498370733e353f1cd0152fa87a5ef2b
rashadul-haque-js <mohammed.haque@iths.se>
2022-04-06 09:00:28 +0200

    fixed a bug related to voters id and memberId

--- public/index.js
+++ public/index.js
@@ -1,15 +1,15 @@
 
 
-let placeholder = document.querySelector('#email')
-let memberEmail = document.querySelector('.member-email').innerHTML
+let input = document.querySelector('#email')
+let memberEmail = document.querySelector('.member-email')
 
 const showEmali = () => {
   
   if (memberEmail) {
-    return placeholder.value = memberEmail
+    return input.value = memberEmail.innerText
   } else {
     console.log('no')
-    return placeholder.value = `type your email`
+    return ''
   }
 
 }
